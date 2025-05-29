2d739ed511cdf5b54de6ec6f6aad504da4abd9be
spotlessswipe <spotlessswipe@pm.me>
2025-05-29 07:50:26 +0200

    serve

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
+)
 
+func main() {
+	serveHandler := http.NewServeMux()
+	server := http.Server{}
+	server.Addr = ":8080"
+	server.Handler = serveHandler
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
