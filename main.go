941c3db44de04043fa0dc5b66d8c23feedbad00d
spotlessswipe <spotlessswipe@pm.me>
2025-05-29 07:56:57 +0200

    better serve

--- main.go
+++ main.go
@@ -6,12 +6,15 @@ import (
 )
 
 func main() {
-	serveHandler := http.NewServeMux()
-	server := http.Server{}
-	server.Addr = ":8080"
-	server.Handler = serveHandler
-	err := server.ListenAndServe()
-	if err != nil {
-		log.Fatal(err)
+	const port = "8080"
+
+	mux := http.NewServeMux()
+
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
 	}
+
+	log.Printf("Serving on port: %s\n", port)
+	log.Fatal(srv.ListenAndServe())
 }
