d581020aa89cd8c73ca8c5022dece682acb7a8d7
spotlessswipe <spotlessswipe@pm.me>
2025-06-02 14:21:43 +0200

    webhooks

--- main.go
+++ main.go
@@ -63,6 +63,7 @@ func main() {
 	mux.HandleFunc("POST /api/login", apiCfg.handlerUserLogin)
 	mux.HandleFunc("POST /api/refresh", apiCfg.handlerRefresh)
 	mux.HandleFunc("POST /api/revoke", apiCfg.handlerRevoke)
+	mux.HandleFunc("POST /api/polka/webhooks", apiCfg.handlerWebhook)
 
 	srv := &http.Server{
 		Addr:    ":" + port,
