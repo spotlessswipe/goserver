921e5e1efc20ad0eceed667c86e3f90560d14152
spotlessswipe <spotlessswipe@pm.me>
2025-06-04 16:53:59 +0200

    update sort

--- handler_chirps_get.go
+++ handler_chirps_get.go
@@ -2,45 +2,40 @@ package main
 
 import (
 	"net/http"
+	"sort"
 
 	"github.com/google/uuid"
 )
 
 func (cfg *apiConfig) handlerChirpsRetrieve(w http.ResponseWriter, r *http.Request) {
-	author_id := r.URL.Query().Get("author_id")
-
-	chirps := []Chirp{}
-
-	if author_id != "" {
-		author_uuid, err := uuid.Parse(author_id)
-		if err != nil {
-			respondWithError(w, http.StatusBadRequest, "Error parsing path to UUID", err)
-			return
-		}
-		dbChirps, err := cfg.db.GetUserChirps(r.Context(), author_uuid)
-		if err != nil {
-			respondWithError(w, http.StatusInternalServerError, "Couldn't retrieve chirps", err)
-			return
-		}
-		for _, dbChirp := range dbChirps {
-			chirps = append(chirps, Chirp{
-				ID:        dbChirp.ID,
-				CreatedAt: dbChirp.CreatedAt,
-				UpdatedAt: dbChirp.UpdatedAt,
-				UserID:    dbChirp.UserID,
-				Body:      dbChirp.Body,
-			})
-		}
-		respondWithJSON(w, http.StatusOK, chirps)
-		return
-	}
 	dbChirps, err := cfg.db.GetChirps(r.Context())
 	if err != nil {
 		respondWithError(w, http.StatusInternalServerError, "Couldn't retrieve chirps", err)
 		return
 	}
 
+	authorID := uuid.Nil
+	authorIDString := r.URL.Query().Get("author_id")
+	if authorIDString != "" {
+		authorID, err = uuid.Parse(authorIDString)
+		if err != nil {
+			respondWithError(w, http.StatusBadRequest, "Invalid author ID", err)
+			return
+		}
+	}
+
+	sortDirection := "asc"
+	sortDirectionParam := r.URL.Query().Get("sort")
+	if sortDirectionParam == "desc" {
+		sortDirection = "desc"
+	}
+
+	chirps := []Chirp{}
 	for _, dbChirp := range dbChirps {
+		if authorID != uuid.Nil && dbChirp.UserID != authorID {
+			continue
+		}
+
 		chirps = append(chirps, Chirp{
 			ID:        dbChirp.ID,
 			CreatedAt: dbChirp.CreatedAt,
@@ -49,6 +44,13 @@ func (cfg *apiConfig) handlerChirpsRetrieve(w http.ResponseWriter, r *http.Reque
 			Body:      dbChirp.Body,
 		})
 	}
+
+	sort.Slice(chirps, func(i, j int) bool {
+		if sortDirection == "desc" {
+			return chirps[i].CreatedAt.After(chirps[j].CreatedAt)
+		}
+		return chirps[i].CreatedAt.Before(chirps[j].CreatedAt)
+	})
 
 	respondWithJSON(w, http.StatusOK, chirps)
 }
