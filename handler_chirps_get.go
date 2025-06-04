bf6189d69e7609101cc25fb3b42f3ff229971198
spotlessswipe <spotlessswipe@pm.me>
2025-06-04 16:45:54 +0200

    userchirps get

--- handler_chirps_get.go
+++ handler_chirps_get.go
@@ -7,13 +7,39 @@ import (
 )
 
 func (cfg *apiConfig) handlerChirpsRetrieve(w http.ResponseWriter, r *http.Request) {
+	author_id := r.URL.Query().Get("author_id")
+
+	chirps := []Chirp{}
+
+	if author_id != "" {
+		author_uuid, err := uuid.Parse(author_id)
+		if err != nil {
+			respondWithError(w, http.StatusBadRequest, "Error parsing path to UUID", err)
+			return
+		}
+		dbChirps, err := cfg.db.GetUserChirps(r.Context(), author_uuid)
+		if err != nil {
+			respondWithError(w, http.StatusInternalServerError, "Couldn't retrieve chirps", err)
+			return
+		}
+		for _, dbChirp := range dbChirps {
+			chirps = append(chirps, Chirp{
+				ID:        dbChirp.ID,
+				CreatedAt: dbChirp.CreatedAt,
+				UpdatedAt: dbChirp.UpdatedAt,
+				UserID:    dbChirp.UserID,
+				Body:      dbChirp.Body,
+			})
+		}
+		respondWithJSON(w, http.StatusOK, chirps)
+		return
+	}
 	dbChirps, err := cfg.db.GetChirps(r.Context())
 	if err != nil {
 		respondWithError(w, http.StatusInternalServerError, "Couldn't retrieve chirps", err)
 		return
 	}
 
-	chirps := []Chirp{}
 	for _, dbChirp := range dbChirps {
 		chirps = append(chirps, Chirp{
 			ID:        dbChirp.ID,
